3291f3de59376c4da4ff21b755477efcd8c251ac
Stephanie Harrison <stephanzonator@hotmail.com>
2021-03-01 22:00:37 +0000

    added Assert Equal code with test cases

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("string", "string");
+assertEqual("string", "string2");
+assertEqual(5 , 5);
+assertEqual(5 , 6);
