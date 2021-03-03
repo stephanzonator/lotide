beb51702eef142803fdc43e71ac479b0ca67bba2
Stephanie Harrison <stephanzonator@hotmail.com>
2021-03-03 23:30:06 +0000

    tested, eslint-ed, added test

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,22 +6,23 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+const bestTVShowsByGenre = {
+  scifi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
 
 const findKeyByValue = function(listObject, show) {
-  for (row in listObject) {
+  for (let row in listObject) {
     if (listObject[row] === show) {
       return row;
     } else {
-      console.log("else log:", listObject[row], " does not equal ", show);
+      // console.log("else log:", listObject[row], " does not equal ", show);
     }
   }
   return undefined;
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
