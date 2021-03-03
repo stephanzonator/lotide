c9cff8425a8ae9e2d0cfb634e96383cc9d614971
Stephanie Harrison <stephanzonator@hotmail.com>
2021-03-03 21:40:44 +0000

    countOnly created, tested, eslint-ed

--- countLetters.js
+++ countLetters.js
@@ -5,11 +5,3 @@ const assertEqual = function(actual, expected) {
     console.log(`Assertion Failed 🛑🛑🛑: ${actual} !== ${expected}`);
   }
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("string", "string");
-assertEqual("string", "string2");
-assertEqual(5 , 5);
-assertEqual(5 , 6);
--- middle.js
+++ middle.js
@@ -24,13 +24,13 @@ const middle = function(arr) {
     return [];
   } else {
     if (arr.length % 2 === 0) {
-      let arrIndex1 = arr.length / 2 - 1 ;
+      let arrIndex1 = arr.length / 2 - 1;
       let arrIndex2 = arr.length / 2;
-      result = [arr[arrIndex1], arr[arrIndex2]]; 
+      let result = [arr[arrIndex1], arr[arrIndex2]];
       return result;
     } else {
       let arrIndex = (arr.length - 1) / 2;
-      result = [arr[arrIndex]]
+      let result = [arr[arrIndex]];
       return result;
     }
   }
