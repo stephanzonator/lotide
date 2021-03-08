12e248135d51e1f0ee6c006e350bf082d5f1221c
Stephanie Harrison <stephanzonator@hotmail.com>
2021-03-08 20:40:31 +0000

    eqArrays Test added

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -18,7 +10,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]) , false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
+module.exports = eqArrays;
