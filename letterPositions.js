4f19f90a271e2400de5884286e241235f06cf013
Stephanie Harrison <stephanzonator@hotmail.com>
2021-03-09 04:53:00 +0000

    letterPositions code modified, testing added

--- letterPositions.js
+++ letterPositions.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  let result = eqArrays(array1, array2);
-  if (result) {
-    console.log(`Assertion Passed: ✅✅✅ ${array1} === ${array2}`);
-  } else {
-    console.log(`Assertion Failed: 🛑🛑🛑 ${array1} !== ${array2}`);
-  }
-};
-
 const letterPositions = function(sentence) {
   let formattedText = sentence.split(" ").join("");
   let results = {};
@@ -39,4 +18,6 @@ const letterPositions = function(sentence) {
 };
 
 
-assertArraysEqual(letterPositions("hello").e, [1]);
+// assertArraysEqual(letterPositions("hello").e, [1]);
+
+module.exports = letterPositions;
